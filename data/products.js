62bffc1cd14be4584f0d25e244003e785e07de4f
BakiForge <bakiforge@gmail.com>
2026-07-10 14:40:02 +0200

    Added few new products and improved product card looking

--- data/products.js
+++ data/products.js
@@ -1,6 +1,6 @@
 export const products = [{
  id: '1',
- image: '../images/nikeshoe.webp',
+ image: '../images/nikeshoe-removebg-preview.png',
  name: 'Nike Air Zoom',
  desc: 'Lightweight running shoes designed for speed and comfort.',
  rating: {
@@ -30,6 +30,105 @@ export const products = [{
   count: '35'
  }, 
  priceCents: 2399
+}, 
+{
+ id: '4',
+ image: '../images/tape-removebg-preview.png',
+ name: 'Fitness Band',
+ desc: 'Achieve your health goals with this sleek, lightweight fitness band.',
+ rating: {
+  stars: '4.5',
+  count: '111'
+ }, 
+ priceCents: 2999
+},
+{
+ id: '5',
+ image: '../images/basketball1.webp',
+ name: 'Basketball',
+ desc: 'Take your game to the next level, with our outstanding basketball.',
+ rating: {
+  stars: '5',
+  count: '410'
+ }, 
+ priceCents: 1699
+}, 
+{
+ id: '6',
+ image: '../images/france-jersey.webp',
+ name: 'Nike France 2026 Home Jersey',
+ desc: 'Support your favorite nation with this jersey.',
+ rating: {
+  stars: '4.5',
+  count: '78'
+ }, 
+ priceCents: 9999
+}, 
+{
+ id: '7',
+ image: '../images/jordan-basketball-removebg-preview.png',
+ name: 'Air Jordan 11 Retro',
+ desc: 'Iconic Jordan basketball shoes.',
+ rating: {
+  stars: '4.0',
+  count: '42'
+ }, 
+ priceCents: 27999
+},
+{
+ id: '8',
+ image: '../images/watch.png',
+ name: 'Sport Watch',
+ desc: 'Track your workouts, heart rate, and daily activity with precision.',
+ rating: {
+  stars: '4.5',
+  count: '71'
+ }, 
+ priceCents: 2099
+},
+{
+ id: '9',
+ image: '../images/bottle.png',
+ name: 'Bottle',
+ desc: 'Stay hydrated during every workout and training sesion.',
+ rating: {
+  stars: '3.5',
+  count: '200'
+ }, 
+ priceCents: 699
+}, 
+{
+ id: '10',
+ image: '../images/wc-ball.webp',
+ name: 'Fifa World Cup 2026 Ball',
+ desc: 'Inspired by the worlds biggest football tournament.',
+ rating: {
+  stars: '5.0',
+  count: '90'
+ }, 
+ priceCents: 6499
+}, 
+{
+ id: '11',
+ image: '../images/wristband.png',
+ name: 'Jordan Wristband',
+ desc: 'Keep sweat away and stay focused during every workout.',
+ rating: {
+  stars: '4.5',
+  count: '15'
+ }, 
+ priceCents: 1299
+},
+{
+ id: '12',
+ image: '../images/gym-bells.png',
+ name: '2 X 6kg Dumbbells',
+ desc: 'Ideal for home workouts, fitness, and strength training.',
+ rating: {
+  stars: '4.5',
+  count: '55'
+ }, 
+ priceCents: 1399
 }
 ];
 
