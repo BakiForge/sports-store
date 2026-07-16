116cd1d2c7f543b2b2d1ec873eec0b9d2f1a43a5
BakiForge <bakiforge@gmail.com>
2026-07-16 14:10:08 +0200

    Add live update for quantity and total price on checkout page

--- scripts/cartPage.js
+++ scripts/cartPage.js
@@ -1,4 +1,14 @@
 import { displayCartHTML, displayTotalHTML } from './displayCart.js';
+import { cart, updateCartQuantity } from "../data/cart.js";
 
 displayCartHTML();
 displayTotalHTML();
+
+window.addEventListener('storage',(event)=>{
+    if(event.key === 'storage') {
+        cart.length = 0;
+        cart.push(...(JSON.parse(localStorage.getItem('cart')) || []));
+    }
+    updateCartQuantity();
+    displayTotalHTML();
+});
